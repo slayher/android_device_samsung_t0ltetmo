bb8b25c64eb025753f6d5c1a3efa8b9f07ceb3fc
slayher <layhertony@gmail.com>
2013-01-13 22:47:36 -0500

    Missed file rename

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ PRODUCT_RELEASE_NAME := t0ltetmo
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/t0ltetmo/device_t0ltetmo.mk)
+$(call inherit-product, device/samsung/t0ltetmo/t0ltetmo.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0ltetmo
